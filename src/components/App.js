6bdf6775fd1b4fabd13532f9196791d560f53cbd
nikgrbn <Rmaps123@gmail.com>
2024-10-19 04:19:21 +0300

    Hooks implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './Header';
 import ContactList from './ContactList';
@@ -6,23 +6,14 @@ import AddContact from './AddContact';
 
 
 function App() {
-
-  const contacts = [
-    {
-    id: '1',
-    name: 'Dhaval',
-    email: 'dhaval@gmail.com'
-    },
-    {
-      id: '2',
-      name: 'Nikesh',
-      email: 'nicks@gmail.com'
-    }
-  ];
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, contact]);
+  }
   return (
     <div className='ui container'>
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts={contacts} />
     </div>
   );
